bed42c01e99772989d376986767b6b0e55226ec5
Gavin Plunkett <gplunkett@lcs.on.ca>
2019-04-08 11:26:15 -0400

    made the Alien move and turn

--- Space Game - Gavin/Alien.java
+++ Space Game - Gavin/Alien.java
@@ -3,17 +3,54 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Alien here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Gavin Plunkett) 
+ * @version (April 8, 2019)
  */
 public class Alien extends Actor
 {
+    // instance variables
+    private int xPosition;
+    private int yPosition;   
+
     /**
      * Act - do whatever the Alien wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-        // Add your action code here.
+        descend();
+
     }    
+    //advance towards the bottom of the world. 
+
+    public void descend()
+    {
+        xPosition = getX();
+        yPosition = getY();
+
+        goDown();
+
+    }
+
+    public void goDown(){
+        if ( isAtEdge() )
+        {
+            randomTurn();
+
+        }
+        else {
+            move(3);
+
+        }
+    }
+
+    public void randomTurn()
+    {
+        if (isAtEdge()) 
+        {
+            turn(Greenfoot.getRandomNumber(180)-45);
+            move(10);
+        }
+
+    }
 }
